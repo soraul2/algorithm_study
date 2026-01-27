d4630b15b7533cb639af23ec888e5c786588eb60
kimtaewoo <goxodn1@gmail.com>
2026-01-27 12:02:05 +0900

    feat: queue 문제를 활용한 카드 문제 풀이 완료. ArrayList만 사용하는것이 오히려 시간적으로 문제일 수 있음. list.remove를 사용하는 경우 맨뒤로 shift를 해주어야하기 때문에 매우 느리다.

--- src/main/java/org/wootae/algorithm/baekjoon/자료구조/카드2164.java
+++ src/main/java/org/wootae/algorithm/baekjoon/자료구조/카드2164.java
@@ -1,4 +1,4 @@
-package org.wootae.algorithm.baekjoon;
+package org.wootae.algorithm.baekjoon.자료구조;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-
+public class 카드2164 {
 
     public static void main(String[] args) throws Exception {
 
@@ -18,17 +17,21 @@ public class Main {
 
         Queue<Integer> queue = new ArrayDeque<>();
 
+        //Queue 는 Inteface , ArrayDeque는 Queue를 상속받은 객체
+        //ArrayDeque를 그대로 사용해도 되지만 ArrayDeque에 있는 함수를 쓰는 것이 없기 때문에 Queue에 있는 poll(Deque) , offer(enque) 를 사용한다.
 
-        for(int i = 1 ; i <= N ; i++){
+        for (int i = 1; i <= N; i++) {
             queue.offer(i);
         }
 
-        while(queue.size() > 1){
+        while (queue.size() > 1) {
             queue.poll();
             queue.offer(queue.poll());
         }
 
         System.out.println(queue.poll());
 
-    }//main
+
+    }
+
 }
